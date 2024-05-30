8016f47eaa51db684b08c28fbd41aec305e5ed68
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-05-30 14:46:44 -0700

    Move plugins.yaml to configs dir, move casc dir under configs dir, update Install-Jenkins to account for moves

--- Install-Jenkins.ps1
+++ Install-Jenkins.ps1
@@ -1,17 +1,18 @@
 # Powershell Core script to install Jenkins locally
 $currentWorkingDir = Get-Location
-$cascJenkinsConfig = "$currentWorkingDir\casc_configs"
+$jenkinsConfigs    = "$currentWorkingDir\configs"
+$jcascConfigs      = "$jenkinsConfigs\casc"
 $jenkinsHome       = "$currentWorkingDir\jenkins"
 $jenkinsWar        = "$jenkinsHome\jenkins.war"
 $jenkinsWarUrl     = "https://github.com/jenkinsci/jenkins/releases/download/jenkins-2.459/jenkins.war"
-$pluginDownloadDir = "$jenkinsHome\plugins"
-$pluginInstallFile = "$cascJenkinsConfig\plugins.yaml"
 $pluginManagerJar  = "$jenkinsHome\jenkins-plugin-manager-2.13.0.jar"
 $pluginManagerUrl  = "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.13.0/jenkins-plugin-manager-2.13.0.jar"
+$pluginsConfig     = "$jenkinsConfigs\plugins.yaml"
+$pluginsDir        = "$jenkinsHome\plugins"
 
 New-Item -ItemType Directory -Force -Path $jenkinsHome
 Invoke-WebRequest $jenkinsWarUrl -OutFile $jenkinsWar
 Invoke-WebRequest $pluginManagerUrl -OutFile $pluginManagerJar
 
-java -jar $pluginManagerJar --war $jenkinsWar --plugin-download-directory $pluginDownloadDir --plugin-file $pluginInstallFile
-java -D"JENKINS_HOME=$jenkinsHome" -D"jenkins.install.runSetupWizard=false" -D"casc.jenkins.config=$cascJenkinsConfig" -jar $jenkinsWar
+java -jar $pluginManagerJar --war $jenkinsWar --plugin-download-directory $pluginsDir --plugin-file $pluginsConfig
+java -D"JENKINS_HOME=$jenkinsHome" -D"jenkins.install.runSetupWizard=false" -D"casc.jenkins.config=$jcascConfigs" -jar $jenkinsWar
