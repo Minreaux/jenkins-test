c1f690251a7a0d716bd85f2ed76e916827e4c9d4
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-05-30 13:21:31 -0700

    Update Install-Jenkins to remove env vars in favor of Java system properties

--- Install-Jenkins.ps1
+++ Install-Jenkins.ps1
@@ -1,19 +1,17 @@
 # Powershell Core script to install Jenkins locally
 $currentWorkingDir = Get-Location
+$cascJenkinsConfig = "$currentWorkingDir\casc_configs"
+$jenkinsHome       = "$currentWorkingDir\jenkins"
 $jenkinsWar        = "$currentWorkingDir\jenkins\jenkins.war"
 $jenkinsWarUrl     = "https://github.com/jenkinsci/jenkins/releases/download/jenkins-2.459/jenkins.war"
+$pluginDownloadDir = "$currentWorkingDir\jenkins\plugins"
 $pluginInstallFile = "$currentWorkingDir\casc_configs\plugins.yaml"
 $pluginManagerJar  = "$currentWorkingDir\jenkins\jenkins-plugin-manager-2.13.0.jar"
 $pluginManagerUrl  = "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.13.0/jenkins-plugin-manager-2.13.0.jar"
 
-# NOTE: Add 'Machine' scope if you want these to persist; requires Administrator privileges
-[System.Environment]::SetEnvironmentVariable('CASC_JENKINS_CONFIG', "$currentWorkingDir\casc_configs")
-[System.Environment]::SetEnvironmentVariable('JENKINS_HOME', "$currentWorkingDir\jenkins")
-[System.Environment]::SetEnvironmentVariable('PLUGIN_DIR', "$currentWorkingDir\jenkins\plugins")
-
-New-Item -ItemType Directory -Force -Path $env:JENKINS_HOME
+New-Item -ItemType Directory -Force -Path $jenkinsHome
 Invoke-WebRequest $jenkinsWarUrl -OutFile $jenkinsWar
 Invoke-WebRequest $pluginManagerUrl -OutFile $pluginManagerJar
 
-java -jar $pluginManagerJar --war $jenkinsWar --plugin-file $pluginInstallFile
-java -D"jenkins.install.runSetupWizard=false" -jar $jenkinsWar
+java -jar $pluginManagerJar --war $jenkinsWar --plugin-download-directory $pluginDownloadDir --plugin-file $pluginInstallFile
+java -D"JENKINS_HOME=$jenkinsHome" -D"jenkins.install.runSetupWizard=false" -D"casc.jenkins.config=$cascJenkinsConfig" -jar $jenkinsWar
