579a8f8fbdc1219f8c4c6ba561168e702f98cbc6
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-05-31 16:57:05 -0700

    Fix syntax errors in testDsl job-dsl script

--- jenkinsfiles/testDsl.groovy
+++ jenkinsfiles/testDsl.groovy
@@ -1,2 +1,4 @@
-println def build_number = "BUILD_NUMBER = ${BUILD_NUMBER}"
-println def branch_name = "BRANCH_NAME = ${BRANCH_NAME}"
+def build_number = "BUILD_NUMBER = ${BUILD_NUMBER}"
+def branch_name = "BRANCH_NAME = ${BRANCH_NAME}"
+println build_number
+println branch_name
