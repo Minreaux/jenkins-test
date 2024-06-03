5e71f8c7f8bd25c1d8b01f84c254912a78e6fb35
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-06-03 15:03:05 -0700

    Setup Job-DSL to seed initial Jenkins pipelines using Job-DSL scripts (#8)
    * Added seedJobs Jenkinsfile
    
    * Added Job-DSL scripts for helloWorld and seedJobs pipelines
    
    * Added jobs.yaml JCasC config used to auto-configure the seedJobs Job-DSL pipeline
    
    * Added "Authorize Project" and "Extended Read Permission" plugins
    
    * Add new "ServiceUser" role and "auto" user in it
    
    * Setup Jenkins to run jobs with "auto" user by default
    
    * Moved JobManager role to be an item role and to hide the "seedJobs" pipeline from these users
    
    * Modified permissions for most roles to have only those permissions required for their intended purpose
    
    * Update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Jenkins Test
 
-This is used for testing Jenkins Configuration as Code (JCasC) and Jenkins Pipeline as Code functionality.
+This is used for testing Jenkins Configuration as Code (JCasC), Jenkins Pipeline as Code, Jenkins Plugin Installation Manager Tool, and Jenkins Job DSL functionality.
 
 ## Getting Started
 Download the repository and run the `Install-Jenkins.ps1` script. This will automatically setup a Jenkins instance locally using configuration as code.
@@ -9,12 +9,15 @@ Once setup has finished, you can access the local Jenkins instance through your 
 
 Mock accounts will be configured automatically and can be used to login. For these accounts, the password is the same as the username:
 
-| User     | Role       |
-| -------- | ---------- |
-| admin    | FullAdmin  |
-| bob      | JobAdmin   |
-| bill     | JobAdmin   |
-| tim      | JobManager |
-| tom      | JobManager |
-| jane     | User       |
-| john     | User       |
+| User     | Role        |
+| -------- | ----------- |
+| admin    | FullAdmin   |
+| bob      | JobAdmin    |
+| bill     | JobAdmin    |
+| tim      | JobManager  |
+| tom      | JobManager  |
+| auto     | ServiceUser |
+| jane     | User        |
+| john     | User        |
+
+> **_NOTE:_**  Jobs are configured to run as the "auto" user by default. Because of this, you must login to the "auto" user once for jobs to work as intended. This is because the "auto" user does not yet exist on first boot, and so jobs are running as "anonmyous" which does not have any permissions. The "auto" user is created after first time login.
---
+++ jenkinsfiles/seedJobs.groovy
@@ -0,0 +1,71 @@
+// Job DSL Groovy script that defines the job configuration for the seedJobs Jenkins pipeline
+// Initial BRANCH_NAME value is passed in through the Jenkins Configuration as Code YAML file
+// Because this is configured as a Multibranch pipeline, BRANCH_NAME can be used in any other Job DSL jobs
+String BRANCH_NAME = "${BRANCH_NAME}" // Prevents getProperty script approval requirement
+multibranchPipelineJob('seedJobs')
+{
+    branchSources
+    {
+        branchSource
+        {
+            source
+            {
+                fromScm
+                {
+                    name(BRANCH_NAME)
+
+                    scm
+                    {
+                        gitSCM
+                        {
+                            branches
+                            {
+                                branchSpec
+                                {
+                                    name(BRANCH_NAME)
+                                }
+                            }
+
+                            browser {}
+
+                            extensions
+                            {
+                                sparseCheckoutPaths
+                                {
+                                    sparseCheckoutPaths
+                                    {
+                                        sparseCheckoutPath
+                                        {
+                                            path('jenkinsfiles/seedJobs')
+                                        }
+                                    }
+                                }
+                            }
+
+                            gitTool('')
+
+                            userRemoteConfigs
+                            {
+                                userRemoteConfig
+                                {
+                                    credentialsId('')
+                                    name('jenkins-test')
+                                    refspec('')
+                                    url('https://github.com/Minreaux/jenkins-test.git')
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    factory
+    {
+        workflowBranchProjectFactory
+        {
+            scriptPath('jenkinsfiles/seedJobs')
+        }
+    }
+}
