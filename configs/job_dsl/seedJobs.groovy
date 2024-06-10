1d7e67c48c8e82f4ce2f2cbf3a5d2f1577c99ecc
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-06-10 16:48:31 -0700

    Rename REPO_URL to GIT_URL

--- configs/job_dsl/seedJobs.groovy
+++ configs/job_dsl/seedJobs.groovy
@@ -1,5 +1,5 @@
 // Job DSL Groovy script that defines the job configuration for the seedJobs Jenkins multibranch pipeline
-// Initial REPO_URL, BRANCH_NAME, and JENKINSFILE_PATH values are passed in through the jobs.yaml Jenkins Configuration as Code file
+// Initial GIT_URL, BRANCH_NAME, and JENKINSFILE_PATH values are passed in through the jobs.yaml Jenkins Configuration as Code file
 // Because this is configured as a Multibranch pipeline, BRANCH_NAME can be used in any other Job DSL scripts that are seeded through this job
 multibranchPipelineJob('seedJobs')
 {
