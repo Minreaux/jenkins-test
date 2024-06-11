c1a714d1e718ead8d52494ffa3ca4982d3594397
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-06-11 14:51:15 -0700

    Update seedJobs pipeline to move GIT_URL and DSL_SCRIPTS to env vars

--- configs/job_dsl/seedJobs.groovy
+++ configs/job_dsl/seedJobs.groovy
@@ -1,4 +1,4 @@
-// Job DSL Groovy script that defines the job configuration for the seedJobs Jenkins multibranch pipeline
+// Job DSL Groovy script that defines the job configuration for a seedJobs Jenkins multibranch pipeline
 // Initial GIT_URL, BRANCH_NAME, and JENKINSFILE_PATH values are passed in through the jobs.yaml Jenkins Configuration as Code file
 // Because this is configured as a Multibranch pipeline, BRANCH_NAME can be used in any other Job DSL scripts that are seeded through this job
 multibranchPipelineJob('seedJobs')
