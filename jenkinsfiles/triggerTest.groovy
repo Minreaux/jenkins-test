dc53f34cc7dab9b1a0d44b9aac63594e46160334
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-08-05 11:21:27 -0700

    Update pipeline with BRANCH_NAME

--- jenkinsfiles/triggerTest.groovy
+++ jenkinsfiles/triggerTest.groovy
@@ -1,5 +1,6 @@
 // Job DSL Groovy script that defines the job configuration for the triggerTest Jenkins pipeline
 // For triggers to be present after seedJobs applies, the trigger must be present in the Job DSL script, not the pipeline Jenkinsfile
+String BRANCH_NAME = "${BRANCH_NAME}" // Prevents getProperty script approval requirement
 pipelineJob('triggerTest')
 {
     definition
@@ -14,7 +15,7 @@ pipelineJob('triggerTest')
 
                 git
                 {
-                    branch('test-job-dsl')
+                    branch(BRANCH_NAME)
                     remote
                     {
                         url('https://github.com/Minreaux/jenkins-test.git')
