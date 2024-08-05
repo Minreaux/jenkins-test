cfeed42167109b073ff2a7977f6585888803484d
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-08-05 10:46:18 -0700

    Update triggerTest Job DSL script

--- jenkinsfiles/triggerTest.groovy
+++ jenkinsfiles/triggerTest.groovy
@@ -1,17 +1,17 @@
 // Job DSL Groovy script that defines the job configuration for the triggerTest Jenkins pipeline
-pipelineJob("triggerTest") 
+pipelineJob('triggerTest')
 {
-    definition 
+    definition
     {
-        cpsScm 
+        cpsScm
         {
-            scm 
+            scm
             {
-                git 
+                git
                 {
                     branch('test-job-dsl')
                     lightweight(true)
-                    remote 
+                    remote
                     {
                         url('https://github.com/Minreaux/jenkins-test.git')
                     }
