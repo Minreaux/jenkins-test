89cf8253d55f932b17821ae73d1d3e31c340b03f
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-08-06 16:16:31 -0700

    Add shared lib to triggerTest DSL

--- jenkinsfiles/triggerTest.groovy
+++ jenkinsfiles/triggerTest.groovy
@@ -1,5 +1,6 @@
 // Job DSL Groovy script that defines the job configuration for the triggerTest Jenkins pipeline
 // For triggers to be present after seedJobs applies, the trigger must be present in the Job DSL script, not the pipeline Jenkinsfile
+@Library('utils')
 String BRANCH_NAME = "${BRANCH_NAME}" // Prevents getProperty script approval requirement
 String CRON_STRING = getCronString(BRANCH_NAME, '* * * * *')
 
