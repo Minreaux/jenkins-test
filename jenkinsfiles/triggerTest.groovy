7d00ee073cda0ea9e9d8796228968c6f7bef5bfb
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-08-06 16:09:11 -0700

    Add getCronString

--- jenkinsfiles/triggerTest.groovy
+++ jenkinsfiles/triggerTest.groovy
@@ -1,6 +1,8 @@
 // Job DSL Groovy script that defines the job configuration for the triggerTest Jenkins pipeline
 // For triggers to be present after seedJobs applies, the trigger must be present in the Job DSL script, not the pipeline Jenkinsfile
 String BRANCH_NAME = "${BRANCH_NAME}" // Prevents getProperty script approval requirement
+String CRON_STRING = getCronString(BRANCH_NAME, '* * * * *')
+
 pipelineJob('triggerTest')
 {
     definition
@@ -35,7 +37,7 @@ pipelineJob('triggerTest')
             {
                 cron
                 {
-                    spec(BRANCH_NAME == 'test-triggers' ? '* * * * *' : '')
+                    spec(CRON_STRING)
                 }
             }
         }
