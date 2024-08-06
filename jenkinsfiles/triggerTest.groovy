0c00af7bdf470566ffe17e91535a1038658ea342
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-08-06 16:18:18 -0700

    Remove getCronString -- cannot get it to work in Job DSL

--- jenkinsfiles/triggerTest.groovy
+++ jenkinsfiles/triggerTest.groovy
@@ -1,9 +1,6 @@
 // Job DSL Groovy script that defines the job configuration for the triggerTest Jenkins pipeline
 // For triggers to be present after seedJobs applies, the trigger must be present in the Job DSL script, not the pipeline Jenkinsfile
-@Library('utils')
 String BRANCH_NAME = "${BRANCH_NAME}" // Prevents getProperty script approval requirement
-String CRON_STRING = getCronString(BRANCH_NAME, '* * * * *')
-
 pipelineJob('triggerTest')
 {
     definition
@@ -38,7 +35,7 @@ pipelineJob('triggerTest')
             {
                 cron
                 {
-                    spec(CRON_STRING)
+                    spec(BRANCH_NAME == 'main' ? '* * * * *' : '')
                 }
             }
         }
--- shared_libraries/utils/vars/getCronString.groovy
+++
@@ -1,4 +0,0 @@
-def call(String BRANCH_NAME, String CRON_STRING)
-{
-    return BRANCH_NAME == 'main' ? CRON_STRING : ''
-}
