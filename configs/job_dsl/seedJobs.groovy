c65d23fd158b5209e905191406e4b3dd11d60c0f
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-08-07 11:27:54 -0700

    Fix typo

--- configs/job_dsl/seedJobs.groovy
+++ configs/job_dsl/seedJobs.groovy
@@ -43,7 +43,7 @@ multibranchPipelineJob('seedJobs')
                             {
                                 sparseCheckoutPaths
                                 {
-                                    spaceCheckoutPath
+                                    sparseeCheckoutPath
                                     {
                                         path(JENKINSFILE_PATH)
                                     }
