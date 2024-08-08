70ad88899f7ee3d91e41491f1eb3ffb8b86aab0d
Minreaux <9372700+Minreaux@users.noreply.github.com>
2024-08-08 09:42:40 -0700

    Update custom seedJobs

--- jenkinsfiles/seedJobs.groovy
+++ jenkinsfiles/seedJobs.groovy
@@ -1,7 +1,8 @@
 // Job DSL Groovy script that defines the job configuration for the seedJobs Jenkins multibranch pipeline
 // This overwrites the initial seedJobs config configured by configs/seedJobs.groovy
 String BRANCH_NAME      = "${BRANCH_NAME}" // Prevents getProperty script approval requirement
-String JENKINSFILE_PATH = 'jenkinsfiles/seedJobs'
+String GIT_URL          = "${GIT_URL}"
+String JENKINSFILE_PATH = "${JENKINSFILE_PATH}"
 multibranchPipelineJob('seedJobs')
 {
     description('This job is used to seed Jenkins jobs through code using Job DSL scripts')
@@ -29,7 +30,7 @@ multibranchPipelineJob('seedJobs')
             {
                 git
                 {
-                    remote('https://github.com/Minreaux/jenkins-test.git')
+                    remote(GIT_URL)
 
                     traits
                     {
